dd2deb9397ac05f6e66cff3e309dd1127c7ee4dd
EwanSmith4001418 <71463995+EwanSmith4001418@users.noreply.github.com>
2023-05-29 22:42:14 +0100

    Changes

--- FPS_DDA_Ewan/Assets/Editor/FOVEditor.cs
+++ FPS_DDA_Ewan/Assets/Editor/FOVEditor.cs
@@ -9,12 +9,12 @@ public class FOVEditor : Editor
     private void OnSceneGUI()
     {
         FieldOfView fov = (FieldOfView)target;
-        Handles.color = Color.black;
-        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360,fov.viewRadius);
-        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2,false);
-        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2,false);
+        //Handles.color = Color.black;
+        //Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360,fov.viewRadius);
+        //Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2,false);
+        //Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2,false);
 
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+        //Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        //Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
     }
 }
